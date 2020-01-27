ae430ae214c22c6d6e2f5358e50e053b46193c93
Roger Esteve <Coconaut7@gmail.com>
2020-01-27 15:45:05 +0100

    Automatic first commit

---
+++ src/cat/esteve/laberint/Start.java
@@ -0,0 +1,9 @@
+package cat.esteve.laberint;
+
+import cat.esteve.laberint.main.Main;
+
+public class Start {
+    public static void main(String[] args) {
+        new Main().start();
+    }
+}
---
+++ src/cat/esteve/laberint/main/Main.java
@@ -0,0 +1,7 @@
+package cat.esteve.laberint.main;
+
+public class Main {
+    public void start() {
+        
+    }
+}
